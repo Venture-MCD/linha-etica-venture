521af67ddc373f5cf66e0ffb6393090ff4a08ba7
Venture-MCD <felipeguidi@venturefastfood.com.br>
2025-10-03 18:37:07 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: { port: 5173, host: true },
-  build: { outDir: 'dist' }
+  base: '/linha-etica-venture/', // <-- caminho do repositório no GitHub Pages
 })

ae39c609fb2dc59153161363df8c9d8fde6d8a19
Venture-MCD <felipeguidi@venturefastfood.com.br>
2025-10-16 15:57:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"; // << TEM QUE EXISTIR
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // se usa Tailwind, mantenha
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
